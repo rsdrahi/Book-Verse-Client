de2cf453902d6691b0c5d5bd428b317528571f3c
rsdrahi <rsdrahi13@gmail.com>
2026-07-11 22:25:25 +0600

    feat: setup project structure and navbar navigation.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,34 @@
+import { navLinks } from "@/constants/navLinks";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="border-b">
+      <nav className="max-w-7xl mx-auto h-16 px-4 flex items-center justify-between">
+
+        {/* logo */}
+        <Link href={'/'}>
+        <h1 className="text-2xl font-bold">Book Verse</h1>
+        </Link>
+
+        {/* menu */}
+        <div>
+          {
+            navLinks.map((link) => (
+              <Link key={link.href} href={link.href}>
+               {link.label}
+              </Link>
+            ))
+          }  
+        </div>
+
+        {/* auth */}
+        <div>
+          Login Register
+        </div>
+
+      </nav>
+    </header>
   );
 };
 
